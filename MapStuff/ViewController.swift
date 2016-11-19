84d7a1dc4b4fa81c3c1c317ca532c34c1c70e1f4
Jeffrey Becker <jeffrey.becker@eagles.oc.edu>
2016-11-18 20:08:19 -0600

    figuring out some map stuff

---
+++ MapStuff/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MapStuff
+//
+//  Created by Lakin Ragains on 11/18/16.
+//  Copyright © 2016 Team Tinker. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.mapType = .Hybrid
+        print(mapView.userLocation)
+        mapView.userTrackingMode = .Follow
+        //var temp = MKDirectionsRequest(contentsOfURL: NSURL())
+        //temp.transportType = .Walking
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
